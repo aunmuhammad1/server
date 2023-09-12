eab86253237ee69a4e34eb0345937111b5de5de7
aunmuhammad <aunmuhammd1>
2023-09-12 04:59:06 -0700

    check payment add

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ app.post('/make-payment', (req, res) => {
     // res.send("Success");
 });
 
+app.get('/check-payment', (req, res) => {
+    MakePayment.find({}).then(data => {
+        console.log(data)
+        res.send(data)
+    }).catch(err => {
+        console.log(err)
+    })
+}
+);
 
 // add code check for existing email
 
@@ -82,53 +91,6 @@ app.post('/send-data', (req, res) => {
 }, (error) => {
     console.log(error);
 });
-
-app.post('/login', (req, res) => {
-    Student.find({
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(data => {
-            console.log(data)
-            if (data.length === 0) {
-                res.send("Invalid")
-            }
-            else {
-                res.send("Success")
-            }
-        }).catch(err => {
-            console.log(err)
-        })
-}
-);
-
-app.post('/update', (req, res) => {
-    Student.findByIdAndUpdate(req.body.id, {
-        name: req.body.name,
-        email: req.body.email,
-        year: req.body.year,
-        password: req.body.password,
-        phone: req.body.phone,
-    })
-        .then(data => {
-            console.log(data)
-            res.send("Success")
-        }).catch(err => {
-            console.log(err)
-        })
-}
-);
-
-app.post('/delete', (req, res) => {
-    Student.findByIdAndRemove(req.body.id)
-        .then(data => {
-            console.log(data)
-            res.send("Success")
-        }).catch(err => {
-            console.log(err)
-        })
-}
-);
 
 
 app.listen(9000, () => {
